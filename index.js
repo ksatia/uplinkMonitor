f3bf0bed7f54ba135c0681a57b51fcc54ea781b4
Karan Satia <karansatia@gmail.com>
2018-06-07 16:03:05 -0400

    decode and log request object payload

--- index.js
+++ index.js
@@ -3,4 +3,44 @@
  */
 
 // Dependencies
-var http = requires('http')
+const http = require('http')
+const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
+
+const server = http.createServer(function (req, res) {
+  // parse URL using request object
+  var parsedURL = url.parse(req.url, true)
+
+  // get the pathName so that we can trim it
+  var urlPathName = parsedURL.pathname
+
+  // trim the value of the pathName object key
+  var trimmedPathName = urlPathName.replace(/^\/+|\/+$/g, '')
+
+  // get the query string
+  var queryString = parsedURL.query
+
+  // get the HTTP method in the request
+  var method = req.method.toUpperCase()
+
+  // parse the request header sent from client
+  var header = req.headers
+
+  // get payload from request object (if there is one)
+  var decoder = new StringDecoder('utf-8')
+  var streamBuffer = ''
+  req.on('data', function (data) {
+    streamBuffer += decoder.write(data)
+  })
+  req.on('end', function (data) {
+    streamBuffer += decoder.end()
+    // send our response back to the client
+    res.end('undercover crispyboi\n')
+    // log out the trimmed path to verify expected output
+    console.log('We got a request with this payload: ', streamBuffer)
+  })
+})
+
+server.listen(process.env.PORT || 8080, function () {
+  console.log('server connected successfully')
+})
