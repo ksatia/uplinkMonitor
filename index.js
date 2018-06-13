d0dc5d6d96a43b3bb5200f4a3c8f5a15cf125ca9
Karan Satia <karansatia@gmail.com>
2018-06-13 16:26:31 -0400

    creates route and handler objects to handle routing logic

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const http = require('http')
 const url = require('url')
 const StringDecoder = require('string_decoder').StringDecoder
 
+// this server would respond to any kind of request from a client
 const server = http.createServer(function (req, res) {
   // parse URL using request object
   var parsedURL = url.parse(req.url, true)
@@ -29,18 +30,62 @@ const server = http.createServer(function (req, res) {
   // get payload from request object (if there is one)
   var decoder = new StringDecoder('utf-8')
   var streamBuffer = ''
+  // bind to event listener for requests specifically (meaning this server is now responding to client requests)
   req.on('data', function (data) {
     streamBuffer += decoder.write(data)
   })
   req.on('end', function (data) {
     streamBuffer += decoder.end()
-    // send our response back to the client
-    res.end('undercover crispyboi\n')
-    // log out the trimmed path to verify expected output
-    console.log('We got a request with this payload: ', streamBuffer)
+
+    // implement router logic
+    var handler = router.hasOwnProperty(trimmedPathName) ? router[trimmedPathName] : handlers.notFound
+
+    // construct object to pass as 'data' parameter for callback
+    var dataObj = {
+      'trimmedPath': trimmedPathName,
+      'query': queryString,
+      'method': method,
+      'header': header,
+      'payload': streamBuffer
+    }
+
+    // route the request to the specified handler
+    // value of the handler variable is a function that takes a data object and a callback
+    handler(dataObj, function (statusCode, payload) {
+      // we can't know the statusCode ahead of time so set a default
+      statusCode = typeof (statusCode) === 'number' ? statusCode : 200
+      payload = typeof (payload) === 'object' ? payload : {}
+      var stringifiedPayload = JSON.stringify(payload)
+
+      // return response to client
+      res.setHeader('content-type', 'application/json')
+      res.writeHead(statusCode)
+      res.end(stringifiedPayload)
+
+      // log out the trimmed path to verify expected output
+      // the statusCode and stringifiedPayload are getting passed in from the handler callback defined below 
+      console.log('Returning this response: ', statusCode, stringifiedPayload)
+    })
   })
 })
+
 
 server.listen(process.env.PORT || 8080, function () {
   console.log('server connected successfully')
 })
+
+// define handler as an empty initialized object
+var handlers = {}
+// write the handler for the 'sample' route
+handlers.sample = function (data, callback) {
+  callback(406, {'name': 'sample handler'})
+}
+
+handlers.notFound = function (data, callback) {
+  callback(404)
+}
+// build a simple router that will call an appropriate handler to serve user the correct content
+// since each path we're going to deal with is unique, we can use an object with unique keys
+const router = {
+  'sample': handlers.sample
+}
