78080070bb6c4276d0347c03d08ab50d520e75ba
Karan Satia <karansatia@gmail.com>
2018-07-16 10:15:12 -0400

    Adds in handler subfunction file and local storage using the fs module for CRUD operations

--- config.js
+++ config.js
@@ -7,16 +7,21 @@
 var environments = {}
 
 environments.staging = {
-  'port': 8080,
+  'httpPort': 8000,
+  'httpsPort': 8001,
   'envName': 'staging'
 }
 
 environments.production = {
-  'port': 3000,
-  'endName': 'production'
+  'httpPort': 3000,
+  'httpsPort': 3001,
+  'envName': 'production'
 }
 
-var passedEnvironment = typeof (process.argv.NODE_ENV) === 'string' ? process.argv.NODE_ENV.toLocaleLowerCase() : ''
-
+var passedEnvironment = typeof (process.env.NODE_ENV) === 'string' ? process.env.NODE_ENV.toLowerCase() : ''
 // check that passed in NODE_ENV value exists in our config container object
-var configEnvironment = environments.hasOwnProperty(passedEnvironment) ? environments.passedEnvironment : environments.staging
+/* we have to use bracket notation because we can't use vars with dot notation in JS otherwise we couldn't know if
+  'passedEnvironment' is a property of environments or a variable called passedEnironment (which it is) */
+var configEnvironment = environments.hasOwnProperty(passedEnvironment) ? environments[passedEnvironment] : environments.staging
+
+module.exports = configEnvironment
--- index.js
+++ index.js
@@ -4,11 +4,39 @@
 
 // Dependencies
 const http = require('http')
+const https = require('https')
 const url = require('url')
 const StringDecoder = require('string_decoder').StringDecoder
+var config = require('./config')
+var fs = require('fs')
+var _data = require('./lib/data')
+var handlers = require('./lib/handlers')
 
 // this server would respond to any kind of request from a client
-const server = http.createServer(function (req, res) {
+const httpServer = http.createServer(function (req, res) {
+  serverLogic(req, res)
+})
+
+// listen using the configuration port passed from the command line
+httpServer.listen(config.httpPort || 5000, function () {
+  console.log('HTTP server connected successfully at port ' + config.httpPort + ' using the ' + config.envName + ' environment')
+})
+
+// we need to create keys that are required for starting an HTTPS server
+var httpsServerOptions = {
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/cert.pem')
+}
+
+const httpsServer = https.createServer(httpsServerOptions, function (req, res) {
+  serverLogic(req, res)
+})
+
+httpsServer.listen(config.httpsPort, function () {
+  console.log('HTTPS server connected successfully at port ' + config.httpsPort + ' using the ' + config.envName + ' environment')
+})
+
+var serverLogic = function (req, res) {
   // parse URL using request object
   var parsedURL = url.parse(req.url, true)
 
@@ -28,14 +56,14 @@ const server = http.createServer(function (req, res) {
   var header = req.headers
 
   // get payload from request object (if there is one)
-  var decoder = new StringDecoder('utf-8')
+  var decoder = new StringDecoder('utf8')
   var streamBuffer = ''
   // bind to event listener for requests specifically (meaning this server is now responding to client requests)
   req.on('data', function (data) {
     streamBuffer += decoder.write(data)
   })
   req.on('end', function (data) {
-    streamBuffer += decoder.end()
+    streamBuffer += decoder.end(data)
 
     // implement router logic
     var handler = router.hasOwnProperty(trimmedPathName) ? router[trimmedPathName] : handlers.notFound
@@ -51,6 +79,7 @@ const server = http.createServer(function (req, res) {
 
     // route the request to the specified handler
     // value of the handler variable is a function that takes a data object and a callback
+    // we're actually invoking the handler here and passing in our data, status code and callback
     handler(dataObj, function (statusCode, payload) {
       // we can't know the statusCode ahead of time so set a default
       statusCode = typeof (statusCode) === 'number' ? statusCode : 200
@@ -63,29 +92,15 @@ const server = http.createServer(function (req, res) {
       res.end(stringifiedPayload)
 
       // log out the trimmed path to verify expected output
-      // the statusCode and stringifiedPayload are getting passed in from the handler callback defined below 
+      // the statusCode and stringifiedPayload are getting passed in from the handler callback defined below
       console.log('Returning this response: ', statusCode, stringifiedPayload)
     })
   })
-})
-
-
-server.listen(process.env.PORT || 8080, function () {
-  console.log('server connected successfully')
-})
-
-// define handler as an empty initialized object
-var handlers = {}
-// write the handler for the 'sample' route
-handlers.sample = function (data, callback) {
-  callback(406, {'name': 'sample handler'})
 }
 
-handlers.notFound = function (data, callback) {
-  callback(404)
-}
 // build a simple router that will call an appropriate handler to serve user the correct content
 // since each path we're going to deal with is unique, we can use an object with unique keys
 const router = {
-  'sample': handlers.sample
+  'ping': handlers.ping,
+  'users': handlers.users
 }
